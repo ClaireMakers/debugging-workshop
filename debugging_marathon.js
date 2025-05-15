8422eaf3e50dbd1735da497fdb1c69fa16a80b2f
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2025-05-15 11:01:26 +0100

    Rewrote workshop to make it more about common errors in common datatypes in JavaScript

--- debugging_marathon.js
+++ debugging_marathon.js
@@ -7,11 +7,11 @@ ignoring anything that's not a sheep */
 const countingSheep = (array) => { 
     let count = 0;
 
-    array.map((element) => { 
-        if (element === "sheep") { 
-            count + 1;
-        }
-    })
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === "sheep") {
+        count + 1;
+      }
+    }
 
     return count;
 }
@@ -23,19 +23,109 @@ if there are any */
 /* For instance, "invisible" should return "nvsble" 2*/
 
 const removingIs = (string) => { 
-    let newString = "";
+    const newString = "";
 
     for (let i = 1; i >= string.length; i++) { 
         if (string[i] != "i" || string[i] != "I") { 
-            newString + string[i];
+            newString += string[i];
         } 
     }  
 
     return newString;
 }
 
+//Could have a simple array problem here maybe?
+
+
 //PROBLEM 3:
+/* Fix the following functions below so that they return the appropriate data. 
+Each function will take the object below as an argument: 
+*/
+
+const cat = {
+  name: "Luna",
+  age: 2,
+  breed: "Bengal",
+  owner: {
+    name: "Tom",
+    contact: {
+      address: {
+        city: "Purrtown",
+      },
+    },
+  },
+  habits: {
+    likes: ["sunbeams", "string"],
+    training: {
+      litterBox: false,
+      },
+  },
+};
+
+/*Here's a link to the material concerning objects in JS and how to access their properties: 
+https://journey.makers.tech/pages/objects*/
+
+// PROBLEM 3A: 
+/*This function should return a string with the cat's likes written out like so: 
+"Luna likes to play with sunbeams and string"
+*/
+function findCatLikes(catObject) {
+    const catLikes = `${catObject.name} likes to play with ${catObject.habits}`;
+    return catLikes;
+}
+
+//Problem 3B:
+/*This function should return a string with the cat's owner details in a string: 
+"Luna's owner is Tom and he lives in Purrtown"
+*/
+function findCatOwnerAddress(catObject) {
+    const catOwnerAddress = `${catObject["name"]}'s owner is ${catObject[owner].name} and he lives in ${catObject[owner][contact]}`
+    return catOwnerAddress;
+}
+
+//Problem 3C: 
+/*For this problem, you'll have to use the two functions below. 
+We want to be able to change whether the cat is litter trained or not, 
+so we will attach a function to the cat object that will let us set it to
+either true or false. The final object will then be: 
+
+const cat = {
+  name: "Luna",
+  age: 2,
+  breed: "Bengal",
+  owner: {
+    name: "Tom",
+    contact: {
+      address: {
+        city: "Purrtown",
+      },
+    },
+  },
+  habits: {
+    likes: ["sunbeams", "string"],
+    training: {
+      litterBox: false,
+      },
+    setLitterBoxTraining: (catObject, boolean) => {
+        //function to toggle the property above between true and false.    
+    }
+  },
+};
+
+*/
+
+function setLitterBoxTraining(catObject, boolean) { 
+    catObject.habits.training.litterBox = boolean;
+} 
+
+function addFieldToObject(catObject, functionToAppend) { 
+    //Here, find a way to create the field setLitterBoxTraining and set it to the function above
+}
+
+//PROBLEM 4:
 /* This function should get rid of each "spy" in the guestsArray, because we want some privacy at our Makers party!
+You can refer to this part of the material for the array.filter() method: https://journey.makers.tech/pages/array-operations
+Or, to the documentation here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 */
 const filteringSpies = (guestsArray) => { 
 
@@ -45,76 +135,15 @@ const filteringSpies = (guestsArray) => {
         }
     } 
 
-    const noSpiesAllowed = guestsArray.filter(filteringCriteria);
-    return noSpiesAllowed; 
+    guestsArray.filter(filteringCriteria);
+    return guestsArray; 
 }
 
-//PROBLEM 4:
-/* This function returns a function that greets a user in a language that was predetermined
-*/
-function createGreeting(greeting) {
-    return function(name) {
-      return greeting + ", " + name + "!"
-    };
-}
- 
-// Update these with a function that returns the correct greeting when called
-// greetSpanish("Leo") => Hola, Leo!
-// greetFrench("Sandra") => Bonjour, Sandra!
-const greetSpanish = null
-const greetFrench = null
+//PROBLEM 5: 
+/**/
+
+module.exports = { countingSheep, removingIs, filteringSpies, findCatLikes, findCatOwnerAddress, addFieldToObject, setLitterBoxTraining };
 
 
-//PROBLEM 5:
-/* This function takes a "person" and return true if a valid UK postcode, and false if not a valid UK postcode and "No postcode found" if postcode not found
-const person = {
-    name: "Alice",
-    address: {
-        street: "123 Main St",
-        city: "Anytown",
-        postcode: "PO5 0LK"
-    }
-};
-*/
-
-const validatePostcode = (postcode) => {
-    // A "regex" tests string to see if matches a given patten. This one validates UK postcodes
-    var regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
-    const isValid = regex.test(postcode);
-    return isValid
-}
-
-
-//PROBLEM 6:
-/* This function takes in an array of "students" and returns an array of students that passed
-
-INPUT
-const testStudents = [
-    { name: "Eve", grade: 70 },
-    { name: "Siddarth", grade: 45 },
-    { name: "Hannah", grade: 85 },
-    { name: "Abdallah", grade: 30 },
-    { name: "Marya", grade: 55 }
-];
-
-OUTPUT
-["Eve", "Hannah", "Marya"]
-*/
-function processStudentsGrades(students) {
-    students.filter(student => student.grade >= 50);
-    const passingStudentNames = students.forEach(student => student.name);
-    return passingStudentNames;
-}
-
-
-
-// Not using "this" in a class
-
-// Misusing Arrays and Object Methods
-
-// Boolean Value Errors
-
-// Scope problems - some function with scope errors and misplaced brackets
-
-
-module.exports = { countingSheep, removingIs, filteringSpies, greetFrench, greetSpanish, validatePostcode, processStudentsGrades };
+//THERE should be an additional repo for the coach, with a problem to show how to debug using console.log
+//since it's really one of the main tools I want them to use here in conjunction with their tests
--- debugging_marathon.test.js
+++ debugging_marathon.test.js
@@ -2,43 +2,104 @@ const {
   countingSheep,
   removingIs,
   filteringSpies,
-  greetSpanish, 
-  greetFrench, 
-  validatePostcode,
-  processStudentsGrades
+  findCatLikes,
+  findCatOwnerAddress,
+  addFieldToObject,
+  setLitterBoxTraining
 } = require("./debugging_marathon");
 
 describe("PROBLEM 1 tests", () => {
-    test("if there is only one sheep in the array, it returns 1", () => {
-        expect(countingSheep(["sheep"])).toBe(1);
+  test("if there is only one sheep in the array, it returns 1", () => {
+    const sheepCount = countingSheep(["sheep"]);
+        expect(sheepCount).toBe(1);
     });
 
-    test("if there is are strings that aren't sheep in the array, it doesn't count them", () => {
-      expect(countingSheep(["sheep", "potato", "sheep"])).toBe(2);
+  test("if there is are strings that aren't sheep in the array, it doesn't count them", () => {
+    const sheepCount = countingSheep(["sheep", "potato", "sheep"]);
+      expect(sheepCount).toBe(2);
     });
 
-    test("if there are no sheep in the array, it returns 0", () => {
-        expect(countingSheep(["apple"])).toBe(0);
+  test("if there are no sheep in the array, it returns 0", () => {
+    const sheepCOunt = countingSheep(["apple"]);
+        expect(sheepCOunt).toBe(0);
     });
 });
 
 describe("PROBLEM 2 tests", () => { 
-    test("If a word doesn't have \"i\" in it, then it returns the word as it is ", () => {
-        expect(removingIs("toothpaste")).toBe("toothpaste");
+  test("If a word doesn't have \"i\" in it, then it returns the word as it is ", () => {
+      const string = removingIs("toothpaste");
+        expect(string).toBe("toothpaste");
     });
 
-    test('If a word has "i"s in it, then it returns the word without them', () => {
-      expect(removingIs("aluminium")).toBe("alumnum");
-    });
-});
-
-describe("PROBLEM 3 tests", () => {
-    test('If there is a "spy" in the array, it should be filtered out', () => {
-      expect(filteringSpies(["spy", "Eddie", "John", "Steve", "Claire"])).toBe(["Eddie", "John", "Steve", "Claire"]);
+  test('If a word has "i"s in it, then it returns the word without them', () => {
+      const string = removingIs("aluminium");
+      expect(string).toBe("alumnum");
     });
 });
 
 describe("PROBLEM 3 tests", () => {
+  const cat = {
+    name: "Luna",
+    age: 2,
+    breed: "Bengal",
+    owner: {
+      name: "Tom",
+      contact: {
+        address: {
+          city: "Purrtown",
+        },
+      },
+    },
+    habits: {
+      likes: ["sunbeams", "string"],
+      training: {
+        litterBox: false,
+      },
+    },
+  };
+
+  test('PROBLEM 3A - it should return the correct string', () => {
+    const string = findCatLikes(cat);
+    expect(string).toBe(
+      "Luna likes to play with sunbeams and string"
+    );
+  });
+
+  test("PROBLEM 3B - it should return the correct string", () => {
+    const string = findCatOwnerAddress(cat)
+    expect(string).toBe(
+      "Luna's owner is Tom and he lives in Purrtown"
+    );
+  });
+
+  test("PROBLEM 3C - it should add the extra field to the original object, and it should be set to the correct function", () => {
+    const updatedObject = addFieldToObject(cat, setLitterBoxTraining);
+    expect(updatedObject).toEqual({
+      name: "Luna",
+      age: 2,
+      breed: "Bengal",
+      owner: {
+        name: "Tom",
+        contact: {
+          address: {
+            city: "Purrtown",
+          },
+        },
+      },
+      habits: {
+        likes: ["sunbeams", "string"],
+        training: {
+          litterBox: false,
+        },
+        setLitterBoxTraining: (catObject, boolean) => {
+          catObject.habits.training.litterBox = boolean;
+        }
+      },
+    });
+  });
+});
+
+describe("PROBLEM 4 tests", () => {
   test('If there is a "spy" in the array, it should be filtered out', () => {
     expect(filteringSpies(["spy", "Eddie", "John", "Steve", "Claire"])).toBe([
       "Eddie",
@@ -47,66 +108,4 @@ describe("PROBLEM 3 tests", () => {
       "Claire",
     ]);
   });
-});
-
-describe("PROBLEM 4 tests", () => {
-  test('greetSpanish should return Hola', () => {
-    expect(greetSpanish("Steve")).toBe("Hola, Steve!");
-  });
-
-  test('greetFrench should return Bonjour', () => {
-    expect(greetFrench("Lisa")).toBe("Bonjour, Lisa!");
-  });
-});
-
-
-describe("PROBLEM 5 tests", () => {
-  test('validatePostcode should return true if a valid UK postcode', () => {
-    const person = {
-      name: "Alice",
-      address: {
-          street: "123 Main St",
-          city: "Anytown",
-          postcode: "HA2 9SW"
-      }
-    };
-    expect(validatePostcode(person)).toEqual(true)
-  });
-
-  test('validatePostcode should return false if not a valid UK postcode', () => {
-    const person2 = {
-      name: "Alicia",
-      address: {
-          street: "123 Main St",
-          city: "Anytown",
-          postcode: "Yaaaaaas"
-      }
-    };
-    expect(validatePostcode(person2)).toEqual(false)
-  });
-
-  test('validatePostcocde should return an error message if no postcode passed in', () => {
-    const person3 = {
-      name: "Alicia",
-      address: {
-          street: "123 Main St",
-          city: "Anytown",
-      }
-    };
-    expect(validatePostcode(person3)).toBe("No postcode found");
-  })
-});
-
-describe("PROBLEM 5 tests", () => {
-  test('validatePostcocde should return an error message if no postcode passed in', () => {
-    const testStudents = [
-      { name: "Eve", grade: 70 },
-      { name: "Siddarth", grade: 45 },
-      { name: "Hannah", grade: 85 },
-      { name: "Abdallah", grade: 30 },
-      { name: "Marya", grade: 55 }
-    ];
-
-    expect(processStudentsGrades(testStudents)).toStrictEqual(['Eve', 'Hannah', 'Marya'])
-  })
 });
